916821263388ce0e659c6551877cd23c53011787
MOSLAH <mohamed.amine.moslah@gmail.com>
2023-04-12 23:45:56 +0200

    advancement 12.04.23

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-spl_autoload_register(function ($className) {
-    $className = str_replace("App", "classes", $className);
-    $className = str_replace("\\", "/", $className);
-    $className .= '.php';
-    require_once $className;
-});
-//require 'classes/Calcul.php';
 
+require "vendor/autoload.php";
 $instance = new App\Calcul();
 
 $resultat = $instance->addtionner(10, 20);
