b63c362d6db650190afd754ede86028f62ee60b8
MOSLAH <mohamed.amine.moslah@gmail.com>
2023-04-23 19:15:11 +0200

    update 23.04.23

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
+use Carbon\Carbon;
 
 require "vendor/autoload.php";
+
 $instance = new App\Calcul();
 
+$aujourdhui = Carbon::now();
+
+var_dump($aujourdhui->format('d/m/Y'));
 $resultat = $instance->addtionner(10, 20);
 
 var_dump($resultat);
